fe293c935a2afcede21f7112937afec2a2667ca3
gabrielscribe <gabriel@scribesecurity.com>
2024-07-29 16:21:47 +0300

    Skips homepage for now

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function HomePage() {
+  
+  
+  // Redirect to the dashboard page
+  const router = useRouter();
+  function redirect() {
+    router.push("/dashboard");
+  }
+  redirect();
 
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
     </div>
-  )
+  );
 }
